2bfee68c53257e960328b4a05b60c0bf48d84d21
Kader AMRICHAT <kader.amrichat@gmail.com>
2022-10-23 23:59:47 +0200

    feat(testing) upgrade the test

--- __TESTS__/index.test.js
+++ __TESTS__/index.test.js
@@ -1,9 +1,9 @@
 const request = require('supertest');
-// const fetch = require('node-fetch');
 const app = require('../app');
 
 it('GET /ping', async () => {
   const res = await request(app).get('/ping');
 
   expect(res.statusCode).toBe(200);
+  expect(res.body.result).toContain('Welcome to simpleRide API');
 });
